e4c15742045af6c4009da3786fdba4f9ba121862
aominhlong <aominhlong@yahoo.com>
2023-05-27 13:14:35 -0700

    Add NavBar component/styling  and remove unused files

---
+++ app/component/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './NavBar.module.css';
+
+function NavBar () {
+    return (
+        <div>
+            Hello World!
+        </div>
+    )
+}
+
+export default NavBar
